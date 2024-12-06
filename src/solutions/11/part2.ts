10a9b191b32812e10e9a71b6864305f8ff01e1c8
Adam Golan <adamski256@gmail.com>
2024-12-06 23:22:43 +0100

    setup boilerlate for all days

---
+++ src/solutions/11/part2.ts
@@ -0,0 +1,3 @@
+import input from "./input.txt?raw";
+
+export const part2 = (ctx: CanvasRenderingContext2D) => {};
---
+++ src/solutions/22/part1.ts
@@ -0,0 +1,3 @@
+import input from "./input.txt?raw";
+
+export const part1 = (ctx: CanvasRenderingContext2D) => {};
