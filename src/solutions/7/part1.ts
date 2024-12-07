8598d4e915ee83ab3b20b73f3d44226efa4dda72
Adam Golan <adamski256@gmail.com>
2024-12-07 11:30:58 +0100

    day 7 without animations

--- src/solutions/7/part1.ts
+++ src/solutions/7/part1.ts
@@ -1,3 +1,42 @@
+import { AnimatedLabel } from "../../helpers/animations/AnimatedLabel";
+import { runAnimation } from "../../helpers/animations/runAnimations";
+import { Vector2 } from "../../helpers/Vector2";
 import input from "./input.txt?raw";
 
-export const part1 = (ctx: CanvasRenderingContext2D) => {};
+export const part1 = (ctx: CanvasRenderingContext2D) => {
+  const equations = input.split("\n");
+  let result = 0;
+
+  for (const equation of equations) {
+    const [testValue, numbers] = equation.split(": ");
+    if (isValid(Number(testValue), numbers.split(" ").map(Number))) {
+      result += Number(testValue);
+    }
+  }
+
+  const middle = new Vector2(ctx.canvas.width / 2, ctx.canvas.height / 2);
+  const resultAnimation = new AnimatedLabel({
+    ctx,
+    label: `Animation in construction... Result for example input: ${result}`,
+    position: middle,
+    opacity: 0,
+  }).animateOpacity(1, 1000);
+
+  runAnimation(ctx, [resultAnimation]);
+};
+
+const isValid = (testValue: number, numbers: number[]): boolean => {
+  let possibleOutcomes = new Set<number>();
+  possibleOutcomes.add(numbers[0]);
+
+  for (const number of numbers.slice(1)) {
+    const newPossibleOutcomes = new Set<number>();
+    for (const possibleOutcome of possibleOutcomes) {
+      newPossibleOutcomes.add(number * possibleOutcome);
+      newPossibleOutcomes.add(number + possibleOutcome);
+    }
+    possibleOutcomes = newPossibleOutcomes;
+  }
+
+  return possibleOutcomes.has(testValue);
+};
