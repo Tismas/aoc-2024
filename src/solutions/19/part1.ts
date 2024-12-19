274e351c57783e035a5e706c04e40e24235e5018
Adam Golan <adamski256@gmail.com>
2024-12-20 00:37:40 +0100

    day 19

--- src/solutions/19/part1.ts
+++ src/solutions/19/part1.ts
@@ -1,2 +1,32 @@
-// @ts-expect-error
-export const part1 = (ctx: CanvasRenderingContext2D, input: string) => {};
+import { showInConstruction } from "../../helpers/animations/showInConstructionMessage";
+
+export const part1 = (ctx: CanvasRenderingContext2D, input: string) => {
+  const [rawAvailablePatterns, rawDesiredPatterns] = input.split("\n\n");
+  const availablePatterns = rawAvailablePatterns.split(", ");
+  const desiredPatterns = rawDesiredPatterns.split("\n");
+  availablePatterns.sort((a, b) => b.length - a.length);
+
+  let result = 0;
+
+  for (const desiredPattern of desiredPatterns) {
+    if (isPatternPossible(desiredPattern, availablePatterns)) {
+      result++;
+    }
+  }
+
+  showInConstruction(ctx, result);
+};
+
+const isPatternPossible = (desiredPattern: string, availablePatterns: string[]): boolean => {
+  if (desiredPattern.length === 0) return true;
+
+  for (const pattern of availablePatterns) {
+    if (desiredPattern.startsWith(pattern)) {
+      if (isPatternPossible(desiredPattern.replace(pattern, ""), availablePatterns)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
--- src/solutions/19/part2.ts
+++ src/solutions/19/part2.ts
@@ -1,2 +1,33 @@
-// @ts-expect-error
-export const part2 = (ctx: CanvasRenderingContext2D, input: string) => {};
+import { showInConstruction } from "../../helpers/animations/showInConstructionMessage";
+
+export const part2 = (ctx: CanvasRenderingContext2D, input: string) => {
+  const [rawAvailablePatterns, rawDesiredPatterns] = input.split("\n\n");
+  const availablePatterns = rawAvailablePatterns.split(", ");
+  const desiredPatterns = rawDesiredPatterns.split("\n");
+  availablePatterns.sort((a, b) => b.length - a.length);
+
+  let result = 0;
+
+  for (const desiredPattern of desiredPatterns) {
+    result += countPossiblePatterns(desiredPattern, availablePatterns);
+  }
+
+  showInConstruction(ctx, result);
+};
+
+const cache: Record<string, number> = {};
+
+const countPossiblePatterns = (desiredPattern: string, availablePatterns: string[]): number => {
+  if (desiredPattern.length === 0) return 1;
+  if (cache[desiredPattern]) return cache[desiredPattern];
+
+  let possiblePatterns = 0;
+  for (const pattern of availablePatterns) {
+    if (desiredPattern.startsWith(pattern)) {
+      possiblePatterns += countPossiblePatterns(desiredPattern.replace(pattern, ""), availablePatterns);
+    }
+  }
+  cache[desiredPattern] = possiblePatterns;
+
+  return possiblePatterns;
+};
