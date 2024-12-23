3bd33c03161979c905e23774081414abbd779c85
Adam Golan <adamski256@gmail.com>
2024-12-23 11:30:36 +0100

    ugly day 23

--- src/solutions/23/part1.ts
+++ src/solutions/23/part1.ts
@@ -1,2 +1,30 @@
-// @ts-expect-error
-export const part1 = (ctx: CanvasRenderingContext2D, input: string) => {};
+import { showInConstruction } from "../../helpers/animations/showInConstructionMessage";
+
+export const part1 = (ctx: CanvasRenderingContext2D, input: string) => {
+  const network: Record<string, string[]> = {};
+
+  for (const line of input.split("\n")) {
+    const [computerA, computerB] = line.split("-");
+    network[computerA] ||= [];
+    network[computerA].push(computerB);
+    network[computerB] ||= [];
+    network[computerB].push(computerA);
+  }
+
+  let threeWithT = new Set<string>();
+  const computersStartingWithT = Object.keys(network).filter((computer) => computer.startsWith("t"));
+  for (const computer of computersStartingWithT) {
+    const connections = network[computer];
+    for (const connection of connections) {
+      const secondConnections = network[connection];
+      for (const secondConnection of secondConnections) {
+        const thirdConnections = network[secondConnection];
+        if (thirdConnections.includes(computer)) {
+          threeWithT.add([computer, connection, secondConnection].sort((a, b) => a.localeCompare(b)).join(","));
+        }
+      }
+    }
+  }
+
+  showInConstruction(ctx, threeWithT.size);
+};
--- src/solutions/23/part2.ts
+++ src/solutions/23/part2.ts
@@ -1,2 +1,51 @@
-// @ts-expect-error
-export const part2 = (ctx: CanvasRenderingContext2D, input: string) => {};
+import { showInConstruction } from "../../helpers/animations/showInConstructionMessage";
+
+export const part2 = (ctx: CanvasRenderingContext2D, input: string) => {
+  const network: Record<string, string[]> = {};
+
+  for (const line of input.split("\n")) {
+    const [computerA, computerB] = line.split("-");
+    network[computerA] ||= [];
+    network[computerA].push(computerB);
+    network[computerB] ||= [];
+    network[computerB].push(computerA);
+  }
+
+  let completeNetworks = new Set<string>(Object.keys(network));
+
+  while (completeNetworks.size > 1) {
+    let largerCompleteNetworks = new Set<string>();
+    for (const completeNetwork of [...completeNetworks]) {
+      const firstNode = completeNetwork.split(",")[0];
+      const candidates = network[firstNode].filter((node) => !completeNetwork.includes(node));
+      for (const candidate of candidates) {
+        const largerNetwork = [...completeNetwork.split(","), candidate];
+        if (isCompleteGraph(largerNetwork, network)) {
+          largerCompleteNetworks.add(largerNetwork.sort((a, b) => a.localeCompare(b)).join(","));
+        }
+      }
+    }
+    completeNetworks = largerCompleteNetworks;
+  }
+
+  showInConstruction(ctx, [...completeNetworks][0]);
+};
+
+const isCompleteGraph = (connectedTo: string[], network: Record<string, string[]>): boolean => {
+  return connectedTo.every((node) =>
+    isConnectedToEveryNode(
+      node,
+      connectedTo.filter((n) => n !== node),
+      network
+    )
+  );
+};
+
+const isConnectedToEveryNode = (node: string, others: string[], network: Record<string, string[]>): boolean => {
+  for (const other of others) {
+    if (!network[other].includes(node)) {
+      return false;
+    }
+  }
+  return true;
+};
